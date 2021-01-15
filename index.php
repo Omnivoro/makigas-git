eef08b47ca7af90a4624d5002479f5bddbab80f6
omnivoro <omnivoro@gmail.com>
2021-01-15 21:08:26 +0000

    Creacion de una franja rojo para mensaje de advertencia

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
 		<p>
 			MiEmpresa S.L. es la empresa líder en el sector de la venta de jarrones chinos. Nuestros clients quedarán encantados con la calidad de los jarrones que importamos desde la mismísima China.
 		</p>
+	<div class="warning">
 		<p>Estamos en contrucción. Perdón por las molestias, vuelvan pronto.</p>
+	</div>
 	</div>
 </body>
 </html>
